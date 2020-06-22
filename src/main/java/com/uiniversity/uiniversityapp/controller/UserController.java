ee7995ec93de226479fe851c70d7b9741dd26bbe
Mateus Gomes <mtsgomesp@gmail.com>
2020-06-22 20:23:45 -0300

    adding login

--- src/main/java/com/uiniversity/uiniversityapp/controller/UserController.java
+++ src/main/java/com/uiniversity/uiniversityapp/controller/UserController.java
@@ -6,6 +6,8 @@ import com.uiniversity.uiniversityapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,6 +18,25 @@ public class UserController {
     
     @Autowired
     private UserService us;
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public ModelAndView loginForm(@ModelAttribute User user) {
+        ModelAndView mv = new ModelAndView();
+        user = us.getUserByLogin(user.getLogin(), user.getPwd());
+        if (user == null) {
+            mv.setViewName("error");
+        }
+        else{
+            mv.setViewName("userView");
+        }
+        mv.addObject("user", user);
+        return mv;
+    }
 
     @GetMapping(value="/user/{login}/{pwd}")
     public ModelAndView getUser(@PathVariable("login") String login,
